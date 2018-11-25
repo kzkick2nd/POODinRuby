4af3653d41e6db95e277317627fb220329ef919d
kzkick2nd <aruga.kazuki@gmail.com>
2018-11-26 01:50:02 +0900

    Add chapter2 codes

---
+++ ch2_4.rb
@@ -0,0 +1,26 @@
+class Gear
+    attr_reader :chainring, :cog, :rim, :tire
+    def initialize(chainring, cog, rim, tire)
+        @chainring = chainring
+        @cog = cog
+        @wheel = Wheel.new(rim,tire)
+    end
+
+    def ratio
+        chainring / cog.to_f
+    end
+
+    def gear_inches
+        ratio * Wheel
+    end
+
+    Wheel = Struct.new(:rim, :tire) do
+        def diameter
+            rim + (tire*2)
+        end
+    end
+end
+
+puts Gear.new(52,11,26,1.5).gear_inches
+puts Gear.new(52,11,24,1.25).gear_inches
+puts Gear.new(52,11).gear_inches
