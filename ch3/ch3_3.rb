7d6e93438b1bf0da997d95c07554f4187de57c06
kzkick2nd <aruga.kazuki@gmail.com>
2018-11-28 00:46:52 +0900

    Add chapter3 codes

---
+++ ch3/ch3_3.rb
@@ -0,0 +1,54 @@
+class Gear
+    attr_reader :chainring, :cog, :wheel
+    def initialize(args)
+        args = defaults.merge(args)
+        @chainring = args[:chainring]
+        @cog       = args[:cog]
+        @wheel     = args[:wheel]
+    end
+
+    def ratio
+        chainring / cog.to_f
+    end
+
+    def gear_inches
+        ratio * diameter
+    end
+
+    def diameter
+        wheel.diameter
+    end
+
+    def defaults
+        {
+            :chainring => 40,
+            :cog => 18
+        }
+    end
+end
+
+class Wheel
+    attr_reader :rim, :tire
+    def initialize(rim, tire)
+        @rim = rim
+        @tire = tire
+    end
+
+    def diameter
+        rim + (tire * 2)
+    end
+
+    def circumference
+        diameter * Math::PI
+    end
+end
+
+puts Gear.new(
+    # :chainring => 52,
+    # :cog => 11,
+    :wheel => Wheel.new(26, 1.5)
+    ).gear_inches
+
+puts Gear.new(
+    :chainring => 52,
+    :cog => 11).ratio
